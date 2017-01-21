8a4729c21e7020d6afaba060a7cf57ef6e80fa9d
mndowne <mndowne@gmail.com>
2017-01-21 15:47:04 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+require('./css/style.css');
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return(
+            <div>
+                Hello World
+            </div>
+        );
+    }
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<App \> , app);
